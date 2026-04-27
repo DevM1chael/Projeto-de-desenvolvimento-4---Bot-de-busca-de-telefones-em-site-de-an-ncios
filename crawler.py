7fe9fd91ad58375655ec4020392357349a782d72
Michael Henrique <michaelhenriquepedro@gmail.com>
2026-04-27 12:07:33 -0300

    feat: implement regex-based phone number extraction and iterate through advertisement links to parse contact details

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 #||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -26,20 +28,46 @@ def parsing(resposta_html):
         print(error)
 
 def encontrar_links(soup):
-    cards_pai = soup.find("div", class_="ui three doubling link cards")
-    cards = cards_pai.find_all("a")
-    
+    try:
+        cards_pai = soup.find("div", class_="ui three doubling link cards")
+        cards = cards_pai.find_all("a")
+    except Exception as error:
+        print("Erro ao extrair os links")
+        return None
+        
     links = []
     for card in cards:
-        link = card['href']
-        links.append(link)
+        try:
+            link = card['href']
+            links.append(link)
+        except:
+            pass
 
     return links
 
+def encontrar_telefone(soup):
+    try:
+        descricao = soup.find_all("div", class_="sixteen wide column") [2].p.get_text().strip()
+    except Exception as error:
+        print("Erro ao extrair o telefone")
+        return None
+    
+    regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", descricao)
+    if regex:
+        return regex
+    
 
-resposta = requisicao(URL_AUTOMOVEIS)
-if resposta:
-    soup = parsing(resposta)
-    if soup:
-        links = encontrar_links(soup)
-        requisicao
+
+
+
+resposta_busca = requisicao(URL_AUTOMOVEIS)
+if resposta_busca:
+    soup_busca = parsing(resposta_busca)
+    if soup_busca:
+        links = encontrar_links(soup_busca)
+        for link in links:
+            resposta_anuncio = requisicao(DOMINIO + link)
+            if resposta_anuncio:
+                soup_anuncio = parsing(resposta_anuncio)
+                if soup_anuncio:
+                    print(encontrar_telefone(soup_anuncio))
