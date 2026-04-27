1b4ceb0cde577a1013936fbac6f8873a3977b2e8
Michael Henrique <michaelhenriquepedro@gmail.com>
2026-04-27 02:45:45 -0300

    chore: add Beautiful Soup 4 dependencies and update crawler implementation

--- crawler.py
+++ crawler.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+#||||||||||||||||||||||||||||||||||||||||||||||||||||
+# Beautiful Soup é uma biblioteca Python usada para extrair dados de arquivos HTML e XML.
+#|||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
 
@@ -6,11 +10,14 @@ def buscar(url):
     try:
         resposta = requests.get(url)
         if resposta.status_code == 200:
-            print(resposta.text)
+            return resposta.text
         else:
             print("Erro ao fazer a requisição")
     except Exception as error:
         print("Erro ao fazer a requisição")
         print(error)
 
-buscar(URL_AUTOMOVEIS)
+resposta = buscar(URL_AUTOMOVEIS)
+if resposta:
+    soup = BeautifulSoup(resposta, 'html.parser')
+    print(soup.prettify())
